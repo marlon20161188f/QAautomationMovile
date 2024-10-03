688a59b454e980e2f76e53d0ccc9ef2b633aa875
Manuel Figueroa <66859042+marlon20161188f@users.noreply.github.com>
2024-10-03 13:20:44 -0500

    create projec

---
+++ src/test/java/com/nttdata/stepsdefinitions/MLibreStepDef.java
@@ -0,0 +1,24 @@
+package com.nttdata.stepsdefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MLibreStepDef {
+    @Given("estoy en la página de MercadoLibre")
+    public void estoyEnLaPáginaDeMercadoLibre() {
+    }
+
+    @When("busco un producto {string}")
+    public void buscoUnProducto(String arg0) {
+    }
+
+    @Then("una lista de resultados")
+    public void unaListaDeResultados() {
+    }
+
+    @And("valido el primer titulo del resultado sea {string}")
+    public void validoElPrimerTituloDelResultadoSea(String arg0) {
+    }
+}
